5aa7a20025157207356ac6ffad4edc12a4cd8d36
tetsumaru <t.nakahara.640103@gmail.com>
2021-08-07 20:16:55 +0900

    対象ファイルのリストを取得できるように修正

--- bulkxl/search_file.py
+++ bulkxl/search_file.py
@@ -1,3 +1,11 @@
-def execute(target_der, exclusion_dir):
-    print(target_der)
-    print(exclusion_dir)
+import glob
+
+
+def execute(target_der, exclusion_dir_list):
+    all_list = glob.glob(target_der + "**/**.xlsx", recursive=True)
+    return [f.replace('\\', '/')
+            for f in all_list if is_target_dir(f, exclusion_dir_list)]
+
+
+def is_target_dir(file_path, exclusion_dir_list):
+    return not any(e in file_path for e in exclusion_dir_list)
