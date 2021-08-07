fc10c65cdfb9cbb3a20eeb17f14b8407bfbb3ff6
tetsumaru <t.nakahara.640103@gmail.com>
2021-08-07 11:07:36 +0900

    formatter導入

--- bulkxl/menu/menu.py
+++ bulkxl/menu/menu.py
@@ -1,4 +1,5 @@
-import tkinter 
+import tkinter
+
 
 def createGui():
     root = tkinter.Tk()
@@ -6,36 +7,36 @@ def createGui():
     root.geometry("400x300")
 
     target_dir_rable = tkinter.Label(root, text="対象ディレクトリ(フルパス)")
-    target_dir_rable.place(x=5,y=0) 
+    target_dir_rable.place(x=5, y=0)
     target_dir_text_box = tkinter.Entry()
     target_dir_text_box.configure(state='normal', width=50)
-    target_dir_text_box.place(x=5,y=20) 
+    target_dir_text_box.place(x=5, y=20)
 
     exclusion_dir_rable = tkinter.Label(root, text="除外対象ディレクトリ名(「,」区切り)")
-    exclusion_dir_rable.place(x=5,y=50) 
+    exclusion_dir_rable.place(x=5, y=50)
     exclusion_dir_text_box = tkinter.Entry()
     exclusion_dir_text_box.configure(state='normal', width=50)
-    exclusion_dir_text_box.place(x=5,y=70) 
+    exclusion_dir_text_box.place(x=5, y=70)
 
     target_sheet_rabel = tkinter.Label(root, text="取得するシート名")
-    target_sheet_rabel.place(x=5,y=100) 
+    target_sheet_rabel.place(x=5, y=100)
     target_sheet_text_box = tkinter.Entry()
     target_sheet_text_box.configure(state='normal', width=50)
-    target_sheet_text_box.place(x=5,y=120) 
+    target_sheet_text_box.place(x=5, y=120)
 
     work_file_rabel = tkinter.Label(root, text="作業用ファイル(フルパス)")
-    work_file_rabel.place(x=5,y=150) 
+    work_file_rabel.place(x=5, y=150)
     work_file_text_box = tkinter.Entry()
     work_file_text_box.configure(state='normal', width=50)
-    work_file_text_box.insert(tkinter.END, u'C:\\temp.xlsx') 
-    work_file_text_box.place(x=5,y=170)
+    work_file_text_box.insert(tkinter.END, u'C:\\temp.xlsx')
+    work_file_text_box.place(x=5, y=170)
 
-    header_record_rabel  = tkinter.Label(root, text="ヘッダー行")
-    header_record_rabel.place(x=5,y=200) 
+    header_record_rabel = tkinter.Label(root, text="ヘッダー行")
+    header_record_rabel.place(x=5, y=200)
     header_record_text_box = tkinter.Entry()
     header_record_text_box.configure(state='normal', width=10)
-    header_record_text_box.insert(tkinter.END, u'0') 
-    header_record_text_box.place(x=5,y=220)
+    header_record_text_box.insert(tkinter.END, u'0')
+    header_record_text_box.place(x=5, y=220)
 
     get_button = tkinter.Button(text='取得', width=10)
     get_button.place(x=5, y=250)
@@ -44,5 +45,6 @@ def createGui():
     update_button.place(x=105, y=250)
 
     root.mainloop()
+
 
 createGui()
